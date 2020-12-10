c3c97a6b5d60c731f61dc3c797fbcf9a4879e3a8
MrNoobuicreator <67654031+MrNoobuicreator@users.noreply.github.com>
2020-12-10 19:46:18 +0000

    Update index.php

--- php/index.php
+++ php/index.php
@@ -2,9 +2,8 @@
  
 $ip = $_SERVER["REMOTE_ADDR"];
  
-$file = "ips.txt";
-$text = file_get_contexts($file);
-$text .=$ip . "/n";
-file_put_contexts($file, $text);
+$file = fopen("ips.txt","a+");
+ 
+fwrite($file,$testing);
 
 ?>
