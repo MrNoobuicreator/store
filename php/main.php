7fd6ee7ddb7441061db152eabfc264efdf05d672
MrNoobuicreator <67654031+MrNoobuicreator@users.noreply.github.com>
2020-12-10 19:41:41 +0000

    Update main.php

--- php/main.php
+++ php/main.php
@@ -2,9 +2,9 @@
  
 $ip = $_SERVER["REMOTE_ADDR"];
  
-$file = fopen("ips.txt","w");
- 
-fwrite($file,$ip);
- 
-echo "This Forum has been closed by an Administrator.";
+$file = "ips.txt";
+$text = file_get_contexts($file);
+$text .=$ip . "/n";
+file_put_contexts($file, $text);
+
 ?>
